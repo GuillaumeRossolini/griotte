27b0b6717ebc6238ff9459eb14f242f390dae6b1
Guillaume Rossolini <g.rossolini@gmail.com>
2024-09-20 19:16:55 +0200

    With better error handling; with a basic graph

--- www/export.php
+++ www/export.php
@@ -1,21 +1,175 @@
 <?php
 
+date_default_timezone_set('Europe/Paris');
+header('Content-Type: text/html; charset=utf-8');
+
 $db = new PDO('sqlite:./readings.sq3');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+/*
+$sql = <<<SQL
+SELECT CASE node
+    WHEN 694510667 THEN 'friends'
+    WHEN 3764971370 THEN 'attic'
+    WHEN 3764978189 THEN 'library'
+    WHEN 1547794758 THEN 'bedroom'
+    WHEN 3385449031 THEN 'kitchen'
+    WHEN 3385445268 THEN 'office'
+    ELSE node
+  END AS node
+  , COUNT(1), MIN(created_at), MAX(created_at)
+  , ROUND(AVG(hpa), 2), MIN(hpa), MAX(hpa)
+  , ROUND(AVG(hum), 2), MIN(hum), MAX(hum)
+  , ROUND(AVG(temp), 2), MIN(temp), MAX(temp)
+  , ROUND(AVG(iaq), 2), MIN(iaq), MAX(iaq)
+  , ROUND(AVG(eco2), 2), MIN(eco2), MAX(eco2)
+  , ROUND(AVG(voc), 2), MIN(voc), MAX(voc)
+FROM sensor_reading
+WHERE node IN (?, ?)
+GROUP BY node, DATE(created_at)
+ORDER BY node, DATE(created_at)
+SQL;
 
-$griotte_nb = 3385449031;
-$created_at = 1726309664;
+$stmt = $db->prepare($sql);
+$stmt->execute([3385449031, 3385445268]);
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($results);
+*/
 
 
 $sql = <<<SQL
-SELECT node
-  , COUNT(1), MIN(created_at), MAX(created_at)
+SELECT CASE node
+    WHEN 694510667 THEN 'friends'
+    WHEN 3764971370 THEN 'attic'
+    WHEN 3764978189 THEN 'library'
+    WHEN 1547794758 THEN 'bedroom'
+    WHEN 3385449031 THEN 'kitchen'
+    WHEN 3385445268 THEN 'office'
+    ELSE node
+  END AS node
+  , COUNT(1) AS nb
+  , MIN(created_at) AS earliest
+  , MAX(created_at) AS latest
+  , ROUND(AVG(hum), 2) AS avg_hum
+  , ROUND(AVG(temp), 2) AS avg_temp
+  , ROUND(AVG(iaq*10), 2) AS avg_iaq
+  , ROUND(AVG(eco2), 2) AS avg_eco2
+  , ROUND(AVG(voc*100), 2) AS avg_voc
 FROM sensor_reading
+WHERE created_at
+  BETWEEN datetime(CURRENT_TIMESTAMP, '-03:00:00')
+  AND datetime(CURRENT_TIMESTAMP, '00:00:00')
 GROUP BY node
+ORDER BY node
 SQL;
 
 $stmt = $db->prepare($sql);
 $stmt->execute();
 
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
+$results = [];
+$labels = [];
+foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $res) {
+  $results[$res['node']] = $res;
+  $labels[] = sprintf('%s (%d)', $res['node'], $res['nb']);
+}
+//print_r($results);
+
+/*
+header('Content-Type: text/plain; charset=utf-8');
+echo json_encode(array_column($results, 'node'));
+echo json_encode(array_map('intval', array_column($results, 'temp')));
+exit;
+*/
+
+$datasets = [
+  'iaq'  => ['label' => 'iAQ',  'color' => '#96f'],
+  'eco2' => ['label' => 'eCO²', 'color' => '#ff9f40'],
+  'voc'  => ['label' => 'VOC',  'color' => '#4bc0c0'],
+];
+
+$tz_utc = new DateTimeZone('UTC');
+$earliest = new DateTimeImmutable(min(array_column($results, 'earliest')), $tz_utc);
+$latest = new DateTimeImmutable(min(array_column($results, 'latest')), $tz_utc);
+
+$tz_local = new DateTimeZone(date_default_timezone_get());
+$title = sprintf(
+  'Overview between %s and %s',
+  $earliest->setTimezone($tz_local)->format('Y-m-d H:i:s'),
+  $latest->setTimezone($tz_local)->format('Y-m-d H:i:s')
+);
+
+$zindex = 0;
+?>
+
+<script src="./chart-v4.4.4.js"></script>
+
+
+<canvas id="overview"></canvas>
+
+<script>
+  new Chart(document.getElementById('overview'), {
+    data: {
+      labels: <?php echo json_encode($labels); ?>,
+      datasets: [
+        <?php foreach($datasets as $field => $config): ?>
+        {
+          type: 'bar',
+          label: '<?=addslashes($config['label'])?>',
+          data: <?php echo json_encode(array_map('intval', array_column($results, 'avg_'.$field))); ?>,
+          borderWidth: 1,
+          weight: 1,
+          order: <?=($zindex--)?>,
+          yAxisID: 'right',
+          borderColor: '<?=$config['color']?>',
+          backgroundColor: '<?=$config['color']?>'
+        },
+        <?php endforeach; ?>
+        {
+          type: 'line',
+          label: 'Temperature (°C)',
+          data: <?php echo json_encode(array_map('intval', array_column($results, 'avg_temp'))); ?>,
+          borderWidth: 1,
+          order: <?=($zindex--)?>,
+          yAxisID: 'left',
+          borderColor: '#ff0000',
+          backgroundColor: '#ff0000'
+        },
+        {
+          type: 'line',
+          label: 'Humidity (%)',
+          data: <?php echo json_encode(array_map('intval', array_column($results, 'avg_hum'))); ?>,
+          borderWidth: 1,
+          order: <?=($zindex--)?>,
+          yAxisID: 'left',
+          borderColor: '#0000ff',
+          backgroundColor: '#0000ff'
+        },
+      ]
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: <?php echo json_encode($title); ?>
+        }
+      },
+      scales: {
+        right: {
+          beginAtZero: true,
+          position: 'right'
+        },
+        left: {
+          beginAtZero: true,
+          position: 'left',
+          suggestedMin: 0,
+          suggestedMax: 100,
+          grid: {
+            display: false
+          }
+        }
+      }
+    }
+  });
+</script>
--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <?php
 
 define('GRIOTTE_STARTTIME', microtime(true));
+header('Content-Type: text/plain; charset=utf-8', true);
 
 register_shutdown_function(function() {
   error_log(sprintf('Script finished after %0.3fms', microtime(true)-GRIOTTE_STARTTIME));
@@ -8,35 +9,31 @@ register_shutdown_function(function() {
 
 
 if(empty($_POST['data'])) {
-  header('Content-Type: text/plain', true, 400);
+  http_response_code(400);
   error_log('L%d: Missing "data" field in the request body', __LINE__);
-  exit;
+  die('ko');
 }
 
 $payload = json_decode($_POST['data'], true);
 if(false === $payload) {
-  header('Content-Type: text/plain', true, 400);
+  http_response_code(400);
   error_log('L%d: Input data was not JSON: %s', __LINE__, print_r($_POST['data'], true));
-  exit;
+  die('ko');
 }
 
 $agent_name = null;
 $griotte_nb = null;
 if(empty($_SERVER['HTTP_USER_AGENT'])) {
-  header('Content-Type: text/plain', true, 400);
+  http_response_code(400);
   error_log(sprintf('L%d: Missing the User-Agent header: %s', __LINE__, json_encode($_SERVER)));
-  exit;
+  die('ko');
 }
 
 if(!preg_match('~(Griotte)/(\d+)$~', $_SERVER['HTTP_USER_AGENT'], $griotte)) {
-  header('Content-Type: text/plain', true, 400);
+  http_response_code(400);
   error_log(sprintf('L%d: Missing the User-Agent header: %s', __LINE__, $_SERVER['HTTP_USER_AGENT']));
-  exit;
+  die('ko');
 }
-
-list($agent_name, $griotte_nb) = $griotte;
-http_response_code(200);
-echo 'ok';
 
 //error_log(sprintf('Response sent after %0.3fms', microtime(true)-GRIOTTE_STARTTIME));
 
@@ -54,17 +51,46 @@ error_log(sprintf('Received payload: %s', $msg));
 
 
 if(!preg_match('~says:\s*(\d+)[^;]+;\s*(\d+)[^;]+;\s*(\d+)[^;]+;\s*([0-9.]+)[^;]+;\s*(\d+)[^;]+;\s*([0-9.]+)[^;]+$~', $msg, $readings)) {
-  header('Content-Type: text/plain', true, 400);
+  http_response_code(400);
   error_log(sprintf('L%d: Unable to match reading pattern', __LINE__));
-  exit;
+  die('ko');
 }
 
 
-$db = new PDO('sqlite:./readings.sq3');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+array_shift($readings);
+http_response_code(200);
+echo 'ok';
+
+
+
+$run_filename = sprintf('/var/run/griotte.%s.run', $griotte_nb);
+$file_exists = file_exists($run_filename);
+
+if(!$file_exists) {
+  //error_log('No readings for this node: '.json_encode($dbg));
+  goto insert;
+}
+
+$filemtime = filemtime($run_filename);
+// error_log(sprintf('File "%s" was modified at %s', $run_filename, date('Y-m-d H:i:s', $filemtime)));
+if(false === $filemtime) {
+  http_response_code(500);
+  error_log(sprintf('L%d: Unable to get file stats: %s', __LINE__, $run_filename));
+  die('ko');
+}
+
+if(time() > ($filemtime + 60*1)) {
+  //error_log('Readings too old for this node: '.json_encode($dbg));
+  goto insert;
+}
+
+
+goto finish;
+
+
+insert:
 
 /*
-
 // maintenance stuff
 
 $sql = <<<SQL
@@ -84,63 +110,32 @@ SQL;
 $db->exec($sql);
 */
 
-
-//$db->exec("DELETE FROM sensor_reading WHERE node IN (101740, 101742, 101744)");
-
-
-$run_filename = sprintf('/var/run/griotte/%d.run', $griotte_nb);
-$file_exists = file_exists($run_filename);
-
-if(!$file_exists) {
-  if(!touch($run_filename)) {
-    header('Content-Type: text/plain', true, 400);
-    error_log(sprintf('L%d: Unable to create file: %s', __LINE__, $run_filename));
-    exit;
-  }
-
-  //error_log('No readings for this node: '.json_encode($dbg));
-  goto insert;
-}
-
-$filemtime = filemtime($run_filename);
-// error_log(sprintf('File "%s" was modified at %s', $run_filename, date('Y-m-d H:i:s', $filemtime)));
-if(false === $filemtime) {
-  header('Content-Type: text/plain', true, 400);
-  error_log(sprintf('L%d: Unable to get file stats: %s', __LINE__, $run_filename));
-  exit;
-}
-
-if(time() > ($filemtime + 60*1)) {
-  //error_log('Readings too old for this node: '.json_encode($dbg));
-  goto insert;
-}
-
-
-goto finish;
-
-
-insert:
-
 $sql = <<<SQL
 INSERT INTO sensor_reading (node, hpa, hum, temp, iaq, eco2, voc)
 VALUES (?, ?, ?, ?, ?, ?, ?)
 SQL;
 
-array_shift($readings);
-$insert = $db->prepare($sql);
-$insert->execute(array_merge([$griotte_nb], $readings));
+try {
+  $db = new PDO('sqlite:./readings.sq3');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $insert = $db->prepare($sql);
+  $insert->execute(array_merge([$griotte_nb], $readings));
+}
+catch(Exception $e) {
+  http_response_code(500);
+  error_log(sprintf('L%d: %s%s%s', __LINE__, $e->getMessage(), PHP_EOL, $e->getTraceAsString()));
+  die('ko');
+}
 
 if(!touch($run_filename)) {
-  header('Content-Type: text/plain', true, 400);
+  http_response_code(500);
   error_log(sprintf('L%d: Unable to create file: %s', __LINE__, $run_filename));
-  exit;
+  die('ko');
 }
 
 error_log(sprintf('Data appended after %0.3fms', microtime(true)-GRIOTTE_STARTTIME));
-//exit;
-
 goto finish;
 
 
 finish:
-exit;  // querying the database takes too long on an SDCard
+exit;
