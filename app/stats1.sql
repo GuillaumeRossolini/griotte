29b972e121017ccce2c28ec1cb37dba5692b6d3e
Guillaume Rossolini <g.rossolini@gmail.com>
2025-06-09 15:46:00 +0000

    Example stats query

---
+++ app/stats1.sql
@@ -0,0 +1,25 @@
+WITH _readings AS (
+  SELECT node, created_at, heap, uptime
+    , LAG (uptime) OVER (PARTITION BY node ORDER BY created_at) AS lag
+  FROM sensor_reading
+  WHERE DATE(created_at) = '2025-06-08'
+)
+, _reboots AS (
+  SELECT node, created_at, lag
+  FROM _readings
+  WHERE uptime < lag
+)
+
+SELECT 
+  DATE(MIN(created_at)) AS day,
+  node,
+  TIME(MIN(created_at)) AS earliest_reading,
+  TIME(MAX(created_at)) AS latest_reading,
+  MIN(heap)/1024 AS heap_ko,
+  MAX(uptime) /60/60 AS uptime_h,
+  COUNT(_reboots.created_at) AS reboots,
+  COUNT(1) AS readings
+FROM _readings
+LEFT JOIN _reboots USING (node, created_at)
+GROUP BY node
+ORDER BY node;
