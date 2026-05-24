ff5930c27e789314132faa230b201d000fe537ee
Guillaume Rossolini <g.rossolini@gmail.com>
2026-05-24 19:21:48 +0200

    RIP the old summary graph at the top of the page, welcome the new data-oriented summary

--- app/graphs.php
+++ app/graphs.php
@@ -32,6 +32,37 @@ unset($date_filter, $match);
     <title><?php echo html('%s metrics for %s', GRIOTTE_LABEL, $start_local->format('Y-m-d')) ?></title>
     <script src="./?script=chart-v4.4.4.js"></script>
   </head>
+
+<style type="text/css">
+  h1 {
+    text-align: center;
+  }
+
+  canvas {
+    margin-top: 50px;
+  }
+
+  table {
+    border: 1px solid black;
+    margin-top: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+  }
+
+  tfoot tr td {
+    font-style: italic;
+  }
+
+  tbody tr td {
+    border: 1px dotted black;
+  }
+
+  td[role="nb"] {
+    text-align: right;
+  }
+</style>
+
 <body>
 
 <form method="get">
@@ -72,8 +103,8 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 /**
- * Start the age with a high-level graph of all the nodes
- * for the chosen date and their average readings
+ * Start the age with a high-level graph of all the nodes for the chosen date,
+ * with their average readings and their health
  */
 
 $config_filename = GRIOTTE_ROOT_PATH.'/app/config.ini';
@@ -93,177 +124,15 @@ foreach($config['node_floors'] as $_idx => $_val) {
 }
 
 $sql_tpl = <<<SQL
-SELECT node
-  , CASE node
-    %s
-    ELSE node
-  END AS node_lbl
-  , CASE node
-    %s
-    ELSE 3
-  END AS floor
-  , COUNT(1) AS nb
-  , MIN(created_at) AS earliest
-  , MAX(created_at) AS latest
-  , ROUND(AVG(hpa/100), 2) AS avg_hpa
-  , ROUND(AVG(hum), 2) AS avg_hum
-  , ROUND(AVG(temp), 2) AS avg_temp
-  , (100 - CASE WHEN ROUND(AVG(iaq/5), 2) > 100 THEN 100 ELSE ROUND(AVG(iaq/5), 2) END) AS avg_iaq
-  , ROUND(AVG(eco2), 2) AS avg_eco2
-  , ROUND(AVG(voc*100), 2) AS avg_voc
-  , ROUND(AVG(accuracy/3*100), 2) AS avg_accuracy
-  , ROUND(AVG(heap), 2) AS avg_heap
-FROM sensor_reading
-WHERE true
-  AND created_at BETWEEN ? AND ?
-GROUP BY node_lbl
-ORDER BY floor, node_lbl
-SQL;
-
-$sql = sprintf($sql_tpl, implode(PHP_EOL, $when_nodes), implode(PHP_EOL, $when_floors));
-
-$stmt = $db->prepare($sql);
-$stmt->execute([
-  $start_utc->format('Y-m-d H:i:s'),
-  $end_utc->format('Y-m-d H:i:s'),
-]);
-
-$overview = [];
-$labels = [];
-foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $res) {
-  $overview[$res['node_lbl']] = $res;
-  $labels[] = sprintf('%s@F%d (%d)', $res['node_lbl'], $res['floor'], $res['nb']);
-}
-
-if(!$overview) {
-  syslog(LOG_ERR, sprintf('No data for %s to %s in %s', $start_utc->format('Y-m-d H:i:s'), $end_utc->format('Y-m-d H:i:s'), $db_filename));
-  ?><h1><?php echo html('No data for %s', $start_local->format('Y-m-d')) ?></h1><?php
-  return;
-}
-
-$datasets = [
-  'eco2' => ['label' => 'eCO²', 'color' => '#ff9f40'],
-  'voc'  => ['label' => 'VOC',  'color' => '#4bc0c0'],
-];
-
-$earliest_raw = min(array_column($overview, 'earliest'));
-$latest_raw = max(array_column($overview, 'latest'));
-
-$earliest = (new DateTimeImmutable($earliest_raw, $tz_utc))
-  ->setTimezone($tz_local);
-
-$latest = (new DateTimeImmutable($latest_raw, $tz_utc))
-  ->setTimezone($tz_local);
-
-$title = sprintf(
-  'Overview on %s between %s and %s',
-  $earliest->format('Y-m-d'),
-  $earliest->format('H:i:s'),
-  $latest->format('H:i:s')
-);
-
-$zindex = 0;
-?>
-
-<canvas id="overview"></canvas>
-
-<script>
-  new Chart(document.getElementById('overview'), {
-    data: {
-      labels: <?php echo json_encode($labels) ?>,
-      datasets: [
-        <?php foreach($datasets as $field => $ds): ?>
-        {
-          type: 'bar',
-          label: <?php echo json_encode($ds['label']) ?>,
-          data: <?php echo json_encode(array_map('intval', array_column($overview, 'avg_'.$field))) ?>,
-          borderWidth: 1,
-          weight: 1,
-          order: <?php echo json_encode($zindex--) ?>,
-          yAxisID: 'right',
-          borderColor: <?php echo json_encode($ds['color']) ?>,
-          backgroundColor: <?php echo json_encode($ds['color']) ?>
-        },
-        <?php endforeach; ?>
-        {
-          type: 'line',
-          label: 'Barometric [hPA]',
-          data: <?php echo json_encode(array_map('intval', array_column($overview, 'avg_hpa'))) ?>,
-          borderWidth: 1,
-          order: <?php echo json_encode($zindex--) ?>,
-          yAxisID: 'right',
-          borderColor: '#c9cbcf',
-          backgroundColor: '#c9cbcf'
-        },
-        {
-          type: 'line',
-          label: 'Temperature [°C]',
-          data: <?php echo json_encode(array_map('intval', array_column($overview, 'avg_temp'))) ?>,
-          borderWidth: 1,
-          order: <?php echo json_encode($zindex--) ?>,
-          yAxisID: 'left',
-          borderColor: '#ff0000',
-          backgroundColor: '#ff0000'
-        },
-        {
-          type: 'line',
-          label: 'Humidity [%]',
-          data: <?php echo json_encode(array_map('intval', array_column($overview, 'avg_hum'))) ?>,
-          borderWidth: 1,
-          order: <?php echo json_encode($zindex--) ?>,
-          yAxisID: 'left',
-          borderColor: '#0000ff',
-          backgroundColor: '#0000ff'
-        },
-        {
-          type: 'line',
-          label: 'IAQ [%]',
-          data: <?php echo json_encode(array_map('intval', array_column($overview, 'avg_iaq'))) ?>,
-          borderWidth: 1,
-          order: <?php echo json_encode($zindex--) ?>,
-          yAxisID: 'left',
-          borderColor: '#96f',
-          backgroundColor: '#96f'
-        }
-      ]
-    },
-    options: {
-      plugins: {
-        title: {
-          display: true,
-          text: <?php echo json_encode($title) ?>
-        }
-      },
-      scales: {
-        right: {
-          beginAtZero: true,
-          position: 'right'
-        },
-        left: {
-          beginAtZero: true,
-          position: 'left',
-          suggestedMin: 0,
-          suggestedMax: 100,
-          grid: {
-            display: false
-          }
-        }
-      }
-    }
-  });
-</script>
-
-
-<?php
-
-/**
- * This is a technical presentation of how healthy the mesh was
- * for the chosen date
- */
-
-$sql_tpl = <<<SQL
 WITH _readings AS (
   SELECT node, created_at, heap, uptime
+    , hpa/100 AS hpa
+    , hum
+    , temp
+    , (100 - CASE WHEN ROUND(iaq/5, 2) > 100 THEN 100 ELSE ROUND(iaq/5, 2) END) AS iaq
+    , eco2
+    , voc*100 AS voc
+    , accuracy/3*100 AS accuracy
     , LAG (uptime) OVER (PARTITION BY node ORDER BY created_at) AS lag
   FROM sensor_reading
   WHERE created_at BETWEEN ? AND ?
@@ -275,21 +144,27 @@ WITH _readings AS (
 )
 
 SELECT
-  node,
-  CASE node
+  node
+  , CASE node
     %s
     ELSE node
-  END AS node_lbl,
-  CASE node
+  END AS node_lbl
+  , CASE node
     %s
     ELSE 3
-  END AS floor,
-  TIME(MIN(created_at)) AS earliest_reading,
-  TIME(MAX(created_at)) AS latest_reading,
-  MIN(heap)/1024 AS heap_ko,
-  MAX(uptime) /60/60 AS uptime_h,
-  COUNT(_reboots.created_at) AS nb_reboots,
-  COUNT(1) AS nb_readings
+  END AS floor
+  , TIME(MIN(created_at)) AS earliest_reading
+  , TIME(MAX(created_at)) AS latest_reading
+  , MIN(heap)/1024 AS heap_ko
+  , MAX(uptime) /60/60 AS uptime_h
+  , COUNT(_reboots.created_at) AS nb_reboots
+  , COUNT(1) AS nb_readings
+  , AVG(hpa) AS avg_hpa
+  , AVG(hum) AS avg_hum
+  , AVG(temp) AS avg_temp
+  , AVG(iaq) AS avg_iaq
+  , AVG(eco2) AS avg_eco2
+  , AVG(voc*100) AS avg_voc
 FROM _readings
 LEFT JOIN _reboots USING (node, created_at)
 GROUP BY node
@@ -306,44 +181,57 @@ $stmt->execute([
 ]);
 
 $health = [];
+$labels = [];
 foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $res) {
-  $health[$res['node']] = $res;
+  $health[$res['node_lbl']] = $res;
+  $labels[] = sprintf('%s@F%d (%d)', $res['node_lbl'], $res['floor'], $res['nb_readings']);
 }
+
+if(!$labels) {
+  syslog(LOG_ERR, sprintf('No data for %s to %s in %s', $start_utc->format('Y-m-d H:i:s'), $end_utc->format('Y-m-d H:i:s'), $db_filename));
+  ?><h1><?php echo html('No data for %s', $start_local->format('Y-m-d')) ?></h1><?php
+  return;
+}
+
+$datasets = [
+  'eco2' => ['label' => 'eCO²', 'color' => '#ff9f40'],
+  'voc'  => ['label' => 'VOC',  'color' => '#4bc0c0'],
+];
+
+$earliest_raw = min(array_column($health, 'earliest_reading'));
+$latest_raw = max(array_column($health, 'latest_reading'));
+
+$earliest = (new DateTimeImmutable($earliest_raw, $tz_utc))
+  ->setTimezone($tz_local);
+
+$latest = (new DateTimeImmutable($latest_raw, $tz_utc))
+  ->setTimezone($tz_local);
+
+$title = sprintf(
+  'Overview on %s between %s and %s',
+  $earliest->format('Y-m-d'),
+  $earliest->format('H:i:s'),
+  $latest->format('H:i:s')
+);
 
 ?>
 
-<style type="text/css">
-  table {
-    border: 1px solid black;
-    margin-top: 20px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 20px;
-  }
-
-  tfoot tr td {
-    font-style: italic;
-  }
-
-  tbody tr td {
-    border: 1px dotted black;
-  }
-
-  td[role="nb"] {
-    text-align: right;
-  }
-</style>
+<h1><?php echo html($title) ?></h1>
 
 <table>
   <thead>
     <tr>
       <th>Node</th>
-      <th>Earliest Reading</th>
-      <th>Latest Reading</th>
-      <th>Heap size</th>
-      <th>Uptime</th>
-      <th>Reboots</th>
+      <th>Earliest</th>
+      <th>Latest</th>
       <th>Readings</th>
+      <th>Heap size</th>
+      <th>Reboots</th>
+      <th>Uptime</th>
+      <th>AVG Atmo</th>
+      <th>AVG Hum</th>
+      <th>AVG Temp</th>
+      <th>AVG Quality</th>
     </tr>
   </thead>
 
@@ -355,13 +243,17 @@ foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $res) {
     ?>
 
     <tr>
-      <td role="nb"><?=html('%d nodes', $nb_nodes)?></td>
+      <td role="nb"><?php echo html('%d nodes', $nb_nodes)?></td>
+      <td></td>
+      <td></td>
+      <td role="nb"><?php echo html('%d total', $nb_readings)?></td>
+      <td></td>
+      <td role="nb"><?php echo html('%d total', $nb_reboots)?></td>
       <td></td>
       <td></td>
       <td></td>
       <td></td>
-      <td role="nb"><?=html('%d total', $nb_reboots)?></td>
-      <td role="nb"><?=html('%d total', $nb_readings)?></td>
+      <td></td>
     </tr>
   </tfoot>
 
@@ -375,13 +267,17 @@ foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $res) {
       ?>
 
       <tr>
-        <td><?=html('%s@F%d', $res['node_lbl'], $res['floor']);?></td>
-        <td role="nb"><?=html($earliest->format('H:i:s'))?></td>
-        <td role="nb"><?=html($latest->format('H:i:s'))?></td>
-        <td role="nb"><?=html('%d Ko', $res['heap_ko'])?></td>
-        <td role="nb"><?=html('%d h', $res['uptime_h'])?></td>
-        <td role="nb"><?=html($res['nb_reboots'])?></td>
-        <td role="nb"><?=html($res['nb_readings'])?></td>
+        <td><?php echo html('%s@F%d', $res['node_lbl'], $res['floor']);?></td>
+        <td role="nb"><?php echo html($earliest->format('H:i:s'))?></td>
+        <td role="nb"><?php echo html($latest->format('H:i:s'))?></td>
+        <td role="nb"><?php echo html($res['nb_readings'])?></td>
+        <td role="nb"><?php echo html('%d Ko', $res['heap_ko'])?></td>
+        <td role="nb"><?php echo html($res['nb_reboots'])?></td>
+        <td role="nb"><?php echo html('%d h', $res['uptime_h'])?></td>
+        <td role="nb"><?php echo html('%d hPa', floor($res['avg_hpa']))?></td>
+        <td role="nb"><?php echo html('%d %%', round($res['avg_hum'], 2))?></td>
+        <td role="nb"><?php echo html('%d °C', round($res['avg_temp'], 2))?></td>
+        <td role="nb"><?php echo html('%d %%', round($res['avg_iaq'], 2))?></td>
       </tr>
     <?php endforeach; ?>
   </tbody>
@@ -419,7 +315,7 @@ $stmt = $db->prepare($sql);
 $sensors = [];
 $labels = [];
 $titles = [];
-foreach($overview as $node_key => $node_average) {
+foreach($health as $node_key => $node_average) {
   $sensors[$node_key] = [];
 
   $stmt->execute([
@@ -460,7 +356,7 @@ foreach($overview as $node_key => $node_average) {
 }
 ?>
 
-<?php foreach($overview as $node_key => $node_average): ?>
+<?php foreach($health as $node_key => $node_average): ?>
   <?php $zindex = 0; ?>
   <canvas id="<?php echo html('room-%s', $node_key) ?>"></canvas>
 
@@ -484,7 +380,7 @@ foreach($overview as $node_key => $node_average) {
           <?php endforeach; ?>
           {
             type: 'line',
-            label: 'Barometric [hPA-920]',
+            label: 'Barometric [hPa-920]',
             data: <?php echo json_encode(array_map('intval', array_column($sensors[$node_key], 'hpa'))) ?>,
             borderWidth: 1,
             order: <?php echo json_encode($zindex--) ?>,
